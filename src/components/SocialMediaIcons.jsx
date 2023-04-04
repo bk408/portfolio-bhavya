4fa4544f06defd8f9cb29e0dad5a3943348f4de2
bk408 <bhavyakhatri1912@gmail.com>
2023-04-04 21:15:35 +0530

    file created

---
+++ src/components/SocialMediaIcons.jsx
@@ -0,0 +1,43 @@
+
+
+
+const SocialMediaIcons = () => {
+    return (
+    <div className="flex justify-center md:justify-start my-10 gap-7">
+        <a
+          className="hover:opacity-50 transition duration-500"
+          href="https://www.linkedin.com/in/bhavya-khatri-12a046131/"
+          target="_blank"
+          rel="noreferrer" 
+        >
+            <img  alt="linkedin-link" src="../assets/linkedin.png"/>
+
+        </a>
+        <a
+          className="hover:opacity-50 transition duration-500"
+          href="https://github.com/bk408"
+          target="_blank"
+          rel="noreferrer" 
+        >
+            <img  alt="github-link" src="../assets/github35.png"/>
+
+        </a>
+        <a
+          className="hover:opacity-50 transition duration-500"
+          href="https://instagram.com/bhavy4313?igshid=ZDdkNTZiNTM="
+          target="_blank"
+          rel="noreferrer" 
+        >
+            <img  alt="instagram-link" src="../assets/instagram.png"/>
+
+        </a>
+        
+
+    </div>
+    )
+
+}
+
+
+
+export default SocialMediaIcons;
